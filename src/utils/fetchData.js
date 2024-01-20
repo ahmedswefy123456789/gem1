8730b015fe1d2edb385a32b5efb08e4a340122f4
tefa1234567 <153877124+tefa1234567@users.noreply.github.com>
2024-01-20 17:04:11 +0200

    project

---
+++ src/utils/fetchData.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+export const exercisesOPtions= {
+    method: 'GET',
+    
+    params: {limit: '10'},
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    }
+};
+
+
+
+
+
+export const fetchData = async (url,options) =>{
+    const response=await fetch(url,options);
+    const data =await response.json();
+
+    return data;
+}
+
+
+
+export const youtubeOptions = {
+    method: 'GET',
+    
+
+    headers: {
+
+        'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
+
+
+
+
+
+
+
+
+
